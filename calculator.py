31d7d99cc8268dc9f2f70455978cb05ee32d4ea0
surekhabaskaran <54308242+surekhabaskaran@users.noreply.github.com>
2020-02-11 12:08:33 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+sum = float(a+b)
+sub = float(a-b)
+mul = a*b
+div = float(a/b)
+print("sum of a and b:",sum)
+print("sub of a and b:",sub)
+print("mul of a and b:",mul)
+print("div of a and b:",div)
